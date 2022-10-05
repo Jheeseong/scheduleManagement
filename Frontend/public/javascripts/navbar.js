88ba836823394fada5f6362e1f46ae6ba94675bc
Bae Dohun <baedormann@gmail.com>
2022-10-05 17:30:39 +0900

    navbar 기능 및 css 보완

--- Frontend/public/javascripts/navbar.js
+++ Frontend/public/javascripts/navbar.js
@@ -3,13 +3,15 @@ document.getElementsByClassName('wrapper')[0].addEventListener('mousedown', clos
 document.getElementById('mainMenu').addEventListener('mousedown', closeSub);
 document.getElementById('subMenu').addEventListener('mousedown', closeSub);
 
+
 //서브메뉴 닫는 함수
 function closeSub(event){
     let targetUser = event.currentTarget.querySelector('.fa-chevron-down')
     let targetColor = event.currentTarget.querySelector('.fa-paint-roller')
+    let targetColorDiv = event.currentTarget.querySelector('.colorDiv')
     let targetSubDiv = event.currentTarget.querySelectorAll('.subDiv')
     console.log(event.target);
-    if(event.target == targetUser || event.target == targetColor){
+    if(event.target == targetUser || event.target == targetColor || event.target == targetColorDiv){
         return;
     }
     for(let i = 0; i < targetSubDiv.length; i++){
@@ -33,6 +35,15 @@ function userSub() {
         userSub.style.display = 'flex';
     }
 }
+
+/* 메뉴색 지정 버튼 나타내기 이벤트리스너 */
+document.getElementById('mainMenu').addEventListener('mouseover', function () {
+    document.getElementsByClassName('colorDiv')[0].style.display = 'flex';
+})
+/* 메뉴색 지정 버튼 감추기 이벤트리스너 */
+document.getElementById('mainMenu').addEventListener('mouseleave', function () {
+    document.getElementsByClassName('colorDiv')[0].style.display = 'none';
+})
 
 function bgSub() {
     document.getElementsByClassName('userSub')[0].style.display = 'none';
