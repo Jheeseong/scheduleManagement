38b6978e02ea924596740b20a48370d4279c9ad3
Bae Dohun <baedormann@gmail.com>
2022-11-03 08:59:11 +0900

    대쉬보드페이지 일정 상태 변경(체크박스)기능 구현 완료 및 sortable 라이브러리 적용

--- Frontend/public/javascripts/Dashboard/Dashboard.js
+++ Frontend/public/javascripts/Dashboard/Dashboard.js
@@ -4,6 +4,70 @@ document.addEventListener('DOMContentLoaded', function () {
     findScheduleList();
 })
 
+function draggable2(){
+    const list = document.querySelectorAll('.scheduleList');
+
+    list.forEach((row) => {
+        row.addEventListener('dragend', () => {
+        })
+        new Sortable(row, {
+            group: "shared",
+            animation: 150,
+            ghostClass: "blue-background-class"
+        });
+    });
+
+}
+/* 일정 드래그 기능을 활성화하는 함수 */
+function draggable() {
+    /* 일정 드래그 */
+    const draggedSchedules = document.querySelectorAll('.scheduleRow');
+    const rowContainers = document.querySelectorAll('.scheduleList');
+
+    draggedSchedules.forEach(draggedSchedule => {
+        draggedSchedule.addEventListener("dragstart", () => {
+            draggedSchedule.classList.add("dragging");
+        });
+
+        draggedSchedule.addEventListener("dragend", () => {
+            draggedSchedule.classList.remove("dragging");
+        });
+    });
+    rowContainers.forEach(rowContainer => {
+        rowContainer.addEventListener("dragover", e => {
+            e.preventDefault();
+            const afterElement = getDragAfterElement(rowContainer, e.clientX);
+            const draggable1 = document.querySelector(".dragging");
+            if (afterElement === undefined) {
+                rowContainer.appendChild(draggable1);
+            } else {
+                rowContainer.insertBefore(draggable1, afterElement);
+            }
+        });
+    });
+}
+
+function getDragAfterElement(container, x) {
+    const draggableElements = [
+        ...container.querySelectorAll(".scheduleRow:not(.dragging)"),
+    ];
+
+    return draggableElements.reduce(
+        (closest, child) => {
+            const box = child.getBoundingClientRect();
+            const offset = x - box.left - box.width / 2;
+            // console.log(offset);
+            if (offset < 0 && offset > closest.offset) {
+                return {offset: offset, element: child};
+            } else {
+                return closest;
+            }
+        },
+        {offset: Number.NEGATIVE_INFINITY},
+    ).element;
+}
+
+
 function findScheduleList() {
     let scheduleList = document.querySelector('.scheduleList')
     let user = document.getElementById('userElement').value
@@ -17,10 +81,12 @@ function findScheduleList() {
             let incomplete = '';
 
             for (let i = 0; i < res.schedule.length; i++) {
-            let str = '';
-                str += '<div class="scheduleRow">';
+                let cbClasses;
+                res.schedule[i].status == true ? cbClasses = 'cb checked' : cbClasses = 'cb';
+                let str = '';
+                str += '<div class="scheduleRow" draggable="true">';
                 str += '<div class="scheduleTitle">';
-                str += '<div class="cb" id="cb' + i + '" onclick="toggleChk(this)"><i class="fa-solid fa-check fa-sm"></i></div>' + res.schedule[i].title;
+                str += '<div class="' + cbClasses + '" id="cb' + i + '" onclick="toggleCb(this, \'' + res.schedule[i]._id + '\')"><i class="fa-solid fa-check fa-2xs"></i></div>' + res.schedule[i].title;
                 str += '</div>';
                 str += '<div>';
                 str += '<i class="fa-regular fa-calendar"></i> ' + setDate(res.schedule[i].startDate) + ' ~ ' + setDate(res.schedule[i].endDate);
@@ -29,14 +95,14 @@ function findScheduleList() {
 
                 if (res.schedule[i].status == true) {
                     complete += str;
-                }
-                else{
+                } else {
                     incomplete += str;
                 }
             }
 
             document.querySelector('.scheduleList.incomplete').innerHTML = incomplete;
             document.querySelector('.scheduleList.complete').innerHTML = complete;
+            draggable2();
         },
         error: function (err) {
             console.log(err)
@@ -44,9 +110,41 @@ function findScheduleList() {
 
     })
 }
-function toggleChk(el){
-    el.classList.toggle('checked');
+
+function toggleCb(cb, id) {
+    let selectedRow = cb.parentElement.parentElement;
+    let completed = document.querySelector('.complete.scheduleList');
+    let incompleted = document.querySelector('.incomplete.scheduleList');
+    let status;
+    cb.classList.toggle('checked');
+
+    if (cb.classList.contains('checked')) {
+        selectedRow.remove();
+        completed.appendChild(selectedRow);
+
+        status = true;
+    } else {
+        selectedRow.remove();
+        incompleted.appendChild(selectedRow);
+
+        status = false;
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: 'schedule/updateStatus/' + id,
+        data: {status: status},
+        dataType: "json",
+        success: function (res) {
+            console.log(res.schedule.status)
+        },
+        error: function (err) {
+            window.alert("일정 상태 변경 실패")
+            console.log(err)
+        }
+    })
 }
+
 function setDate(date) {
     let dateSplitArr = date.split('T');
     let toDate = dateSplitArr[0];
@@ -96,17 +194,17 @@ function categoryList() {
             res.categories.map((category) => {
                 str += '<div class="categoryRow">' +
                     '<div class="categoryContent">' +
-                    '<div class="categoryTitle"><span>'+ category.title +'</span></div>' +
+                    '<div class="categoryTitle"><span>' + category.title + '</span></div>' +
                     '<div class="categoryBottom">' +
                     '<div class="categoryTag">'
                 category.tagInfo.map((tag, index) => {
-                    str+= '<span>'+ tag.content +'</span>';
+                    str += '<span>' + tag.content + '</span>';
                     index++
                 })
                 str += '</div>' +
                     '<div class="categoryCreator">' +
-                    '<img class="userImage" src="'+ category.creator.image +'">' +
-                    '<p>'+ category.creator.name+'</p>' +
+                    '<img class="userImage" src="' + category.creator.image + '">' +
+                    '<p>' + category.creator.name + '</p>' +
                     '</div></div></div>' +
                     '</div>'
             })
@@ -118,3 +216,4 @@ function categoryList() {
 
     })
 }
+
